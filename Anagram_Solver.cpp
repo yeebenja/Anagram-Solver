00977f93402a50a235d739ae1ce8328a47b0275e
Benjamin Yee <95982168+yeebenja@users.noreply.github.com>
2023-03-03 20:16:11 -0500

    Add files via upload

---
+++ Anagram_Solver.cpp
@@ -0,0 +1,61 @@
+// Anagram Solver (for GamePigeon iMessage Anagram Game)
+// By: Benjamin Yee (Yu Anmin / Yeo Anmin)
+// Email: yeebenja@umich.edu
+// LinkedIn: https://www.linkedin.com/in/yeebenja/
+// GitHub: https://github.com/yeebenja
+
+#include <iostream>
+#include <getopt.h>
+#include <map>
+#include <queue>
+
+
+using namespace std;
+
+//// Option enumerated classes
+//enum class Cardinal_Output_Mode { kNone = 0, k_Cardinal_Mode_On, };					 // -c
+//enum class Index_Output_Mode { kNone = 0, k_Index_Mode_On, };						 // -i
+//enum class Point_Output_Mode { kNone = 0, k_Point_Mode_On, };						 // -p
+//enum class Alpha_Output_Mode { kNone = 0, k_Alpha_Mode_On, };					     // -a
+//enum class Search_Depth_Output_Mode { kNone = 0, k_Search_Depth_Mode_On, };			 // -s
+//enum class Board_Mode { kNone = 0, k_Board_Mode_On, };								 // -b
+//enum class Linear_Search_Only_Mode { kNone = 0, k_Linear_Search_Mode_On, };			 // -l
+//enum class Word_Hunt_Mode { kNone = 0, k_Word_Hunt_Mode_On, };						 // -w
+//enum class Specify_Length_Mode { kNone = 0, k_Specify_Length_Mode_on, };			 // -x
+//enum class Specify_First_Letter_Mode { kNone = 0, k_Specify_First_Letter_Mode_On, }; // -y
+//
+//
+//// Options struct
+//struct Options {
+//	Cardinal_Output_Mode cardinal_output_mode = Cardinal_Output_Mode::kNone;
+//	Index_Output_Mode index_output_mode = Index_Output_Mode::kNone;
+//	Point_Output_Mode point_output_mode = Point_Output_Mode::kNone;
+//	Alpha_Output_Mode alpha_output_mode = Alpha_Output_Mode::kNone;
+//	Search_Depth_Output_Mode search_depth_output_mode = Search_Depth_Output_Mode::kNone;
+//	Board_Mode board_mode = Board_Mode::kNone;
+//	Linear_Search_Only_Mode linear_mode = Linear_Search_Only_Mode::kNone;
+//	Word_Hunt_Mode word_hunt_mode = Word_Hunt_Mode::kNone;
+//	Specify_Length_Mode specify_length_mode = Specify_Length_Mode::kNone;
+//	Specify_First_Letter_Mode specify_first_letter_mode = Specify_First_Letter_Mode::kNone;
+//
+//	int search_depth = 6;					// Default Search Depth value is 6
+//	string board_filename = "board.txt";	// Default Board filename is "board.txt"
+//	int specified_length = 0;
+//	vector<char> specified_first_letters;
+//};
+
+
+class Anagram_Solver {
+private:
+
+
+
+
+public:
+	// EFFECTS: Runs Anagram Solver object
+	void run(void) {
+
+	}
+};
+
+
